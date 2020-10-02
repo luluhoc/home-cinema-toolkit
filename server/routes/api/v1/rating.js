50a43c1e9aac2297e4ef1be06631587a506a79ee
Lucjan Grzesik <lucjan.grzesik@gmail.com>
2020-10-02 10:54:43 +0200

    settings

--- server/routes/api/v1/rating.js
+++ server/routes/api/v1/rating.js
@@ -6,7 +6,7 @@ import low from 'lowdb';
 
 const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('db.json');
+const adapter = new FileSync('db/rating.json');
 const db = low(adapter);
 
 // router
--- server/routes/api/v1/settings.js
+++ server/routes/api/v1/settings.js
@@ -6,7 +6,7 @@ import low from 'lowdb';
 // DB CONFIG
 const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('db.json');
+const adapter = new FileSync('db/settings.json');
 const db = low(adapter);
 
 // router
