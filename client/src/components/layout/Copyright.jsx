ffb5208ef3f23fb50d1868eef60b82562da1f75a
Lucjan Grzesik <lucjan.grzesik@gmail.com>
2020-10-14 10:39:10 +0200

    version indication and verbose remove

--- client/src/components/layout/Copyright.jsx
+++ client/src/components/layout/Copyright.jsx
@@ -3,6 +3,7 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 
 const Copyright = () => (
+  <>
   <Typography variant="body2" color="textSecondary" align="center">
     {'Copyright © '}
     <Link color="inherit" href="https://github.com/luluhoc" target="_blank" component="a">
@@ -12,6 +13,8 @@ const Copyright = () => (
     {new Date().getFullYear()}
     .
   </Typography>
+  <Typography variant="body2" color="textSecondary" align="center">v.1.0.1</Typography>
+  </>
 );
 
 export default Copyright;
