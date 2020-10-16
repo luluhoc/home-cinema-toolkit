388d42cc54a13fe3783d5d5891b010f86c268f2d
Lucjan Grzesik <lucjan.grzesik@gmail.com>
2020-10-16 19:22:30 +0200

    error handle

--- server/routes/api/v1/rating.js
+++ server/routes/api/v1/rating.js
@@ -48,8 +48,14 @@ router.post('/', async (req, res) => {
     console.log('Got movies from radarr...');
   } catch (error) {
     console.log(error);
+    if (error && error.response && error.response.status === 404) {
+      return res.status(404).json({ errors: [{ msg: '404 NOT FOUND - Probably bad radarr link' }] });
+    }
     if (error && error.code === 'ETIMEDOUT') {
       return res.status(408).json({ errors: [{ msg: error.code }] });
+    }
+    if (error && error.code === 'ENOTFOUND') {
+      return res.status(404).json({ errors: [{ msg: `404 NOT FOUND - Probably bad radarr link - ${error.code}` }] });
     }
     if (error && error.response && error.response.status === 401) {
       return res.status(401).json({ errors: [{ msg: error.response.statusText }] });
