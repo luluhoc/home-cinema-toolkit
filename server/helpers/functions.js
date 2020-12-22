440c183ffa251f5f08c5eadcb909d365a0e17c9d
Lucjan Grzesik <lucjan.grzesik@gmail.com>
2020-12-22 18:13:03 +0100

    refactor

--- server/helpers/functions.js
+++ server/helpers/functions.js
@@ -2,6 +2,9 @@
 import normalizeUrl from 'normalize-url';
 import low from 'lowdb';
 import axios from 'axios';
+import {
+  sleep,
+} from './index';
 
 const FileSync = require('lowdb/adapters/FileSync');
 
@@ -121,6 +124,100 @@ export const getMoviesFromRadarr = async () => {
   };
 };
 
-export const getRatingFromOmdb = () => {
+export const getRatingFromOmdb = async (req) => {
+  const { io } = req.app.locals;
+  db.read();
+  dbs.read();
+  const settings = await dbs.get('settings').value();
+  if (!settings || !settings.keyOmdb || !settings.radarrUrl || !settings.radarrApi
+    || settings.deleteFiles === undefined || settings.addExclusion === undefined) {
+    return {
+      error: true,
+      errors: [{
+        msg: 'No settings',
+      }],
+      code: 400,
+    };
+  }
+  const { keyOmdb } = settings;
+  console.log('Searching movies in OMDB...');
+  const moviesFromDb = db.get('movies').value();
+  console.log(`DB LENGTH ${moviesFromDb.length}`);
+  let progress = 0;
+  io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+  });
+  const oneMovieProgress = 100 / moviesFromDb.length;
+  const diff = 43800;
+  const func = async (m) => {
+    await sleep(1);
+    const d = await axios(`http://www.omdbapi.com/?apikey=${keyOmdb}&i=${m}`);
+    let b = 0;
+    if (!d) {
+      console.error('Error with response from OMDB');
+      return;
+    }
+    if (d.data && d.data.imdbVotes) {
+      b = parseFloat(d.data.imdbVotes.replace(/,/g, ''));
+    }
+    let g = [];
+    if (d.data && d.data.Genre) {
+      g = d.data.Genre.split(',').map((item) => item.trim());
+    }
+    let i = 0;
+    if (d.data && d.data.imdbID) {
+      i = d.data.imdbID;
+    }
+    let rat = 0;
+    if (d.data && d.data.imdbRating) {
+      rat = d.data.imdbRating;
+    }
+    let pos = '';
+    if (d.data && d.data.Poster) {
+      pos = d.data.Poster;
+    }
 
+    await db.get('movies')
+      .find({
+        imdbId: i,
+      })
+      .assign({
+        imdbVotes: b,
+        imdbRating: rat,
+        Genre: g,
+        Poster: pos,
+        expires: new Date(new Date().getTime() + diff * 60000),
+      })
+      .write();
+    io.emit('FromAPI', d.data.Title);
+    progress += oneMovieProgress;
+    io.emit('Progress', progress);
+  };
+  for (let index = 0; index < moviesFromDb.length; index += 1) {
+    const a = moviesFromDb[index];
+    try {
+      if (a && !a.expires) {
+        // eslint-disable-next-line no-await-in-loop
+        await func(a.imdbId);
+      } else if (a && Date.parse(a.expires) < new Date()) {
+        // eslint-disable-next-line no-await-in-loop
+        await func(a.imdbId);
+      } else {
+        continue;
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.code === 'ECONNRESET') {
+        await func(a.imdbId);
+      }
+    }
+  }
+  return {
+    error: false,
+    errors: [],
+    code: 200,
+  };
 };
--- server/routes/api/v1/rating.js
+++ server/routes/api/v1/rating.js
@@ -7,10 +7,7 @@ import {
   check,
   validationResult,
 } from 'express-validator';
-import {
-  sleep,
-} from '../../../helpers/index';
-import { getMoviesFromRadarr } from '../../../helpers/functions';
+import { getMoviesFromRadarr, getRatingFromOmdb } from '../../../helpers/functions';
 // DB CONFIG
 const FileSync = require('lowdb/adapters/FileSync');
 
@@ -46,100 +43,12 @@ router.post('/', [
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  const { io } = req.app.locals;
-  db.read();
-  dbs.read();
-  const settings = await dbs.get('settings').value();
-  if (!settings || !settings.keyOmdb || !settings.radarrUrl || !settings.radarrApi
-    || settings.deleteFiles === undefined || settings.addExclusion === undefined) {
-    return res.status(400).json({
-      errors: [{
-        msg: 'No settings',
-      }],
-    });
+  const obj = await getRatingFromOmdb(req);
+  if (obj && obj.error) {
+    return res.status(obj.code).json({ errors: obj.errors });
   }
-  const { keyOmdb } = settings;
   const desiredRating = Number(req.body.desiredRating);
-  console.log('Searching movies in OMDB...');
-  const moviesFromDb = db.get('movies').value();
-  console.log(`DB LENGTH ${moviesFromDb.length}`);
-  let progress = 0;
-  io.on('connection', (socket) => {
-    console.log('New client connected');
-    socket.on('disconnect', () => {
-      console.log('Client disconnected');
-      return res.status(500).json({
-        errors: [{
-          msg: 'Server Error - ended on disconnect',
-        }],
-      });
-    });
-  });
-  const oneMovieProgress = 100 / moviesFromDb.length;
-  const diff = 43800;
-  const func = async (m) => {
-    await sleep(1);
-    const d = await axios(`http://www.omdbapi.com/?apikey=${keyOmdb}&i=${m}`);
-    let b = 0;
-    if (!d) {
-      console.error('Error with response from OMDB');
-      return;
-    }
-    if (d.data && d.data.imdbVotes) {
-      b = parseFloat(d.data.imdbVotes.replace(/,/g, ''));
-    }
-    let g = [];
-    if (d.data && d.data.Genre) {
-      g = d.data.Genre.split(',').map((item) => item.trim());
-    }
-    let i = 0;
-    if (d.data && d.data.imdbID) {
-      i = d.data.imdbID;
-    }
-    let rat = 0;
-    if (d.data && d.data.imdbRating) {
-      rat = d.data.imdbRating;
-    }
-    let pos = '';
-    if (d.data && d.data.Poster) {
-      pos = d.data.Poster;
-    }
-
-    await db.get('movies')
-      .find({
-        imdbId: i,
-      })
-      .assign({
-        imdbVotes: b,
-        imdbRating: rat,
-        Genre: g,
-        Poster: pos,
-        expires: new Date(new Date().getTime() + diff * 60000),
-      })
-      .write();
-    io.emit('FromAPI', d.data.Title);
-    progress += oneMovieProgress;
-    io.emit('Progress', progress);
-  };
-  for (let index = 0; index < moviesFromDb.length; index += 1) {
-    const a = moviesFromDb[index];
-    try {
-      if (a && !a.expires) {
-        // eslint-disable-next-line no-await-in-loop
-        await func(a.imdbId);
-      } else if (a && Date.parse(a.expires) < new Date()) {
-        // eslint-disable-next-line no-await-in-loop
-        await func(a.imdbId);
-      } else {
-        continue;
-      }
-    } catch (error) {
-      console.log(error);
-      if (error.code === 'ECONNRESET') {
-        await func(a.imdbId);
-      }
-    }
-  }
+  db.read();
   console.log('Sending movies to Frontend');
   try {
     const returnedMovies = db.get('movies').filter((movie) => movie.imdbRating <= desiredRating);
